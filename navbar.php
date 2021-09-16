38268d934372728731aad821cbc5acb62b01f757
unknown <ibnu.setia23@gmail.com>
2021-09-16 19:06:35 +0800

    oke

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<nav class="fh5co-nav" role="navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div id="fh5co-logo"><a href="index.html">Ibnu & Jahrah<strong>.</strong></a></div>
+			</div>
+			<div class="col-sm-6 text-right menu-1">
+				<ul>
+					<li class="active"><a href="index.html">Home</a></li>
+					<li><a href="about.html">Story</a></li>
+					<li class="has-dropdown">
+						<a href="gallery.html">Gallery</a>
+						<ul class="dropdown">
+							<li><a href="#">HTML5</a></li>
+							<li><a href="#">CSS3</a></li>
+							<li><a href="#">Sass</a></li>
+							<li><a href="#">jQuery</a></li>
+						</ul>
+					</li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+</nav>
